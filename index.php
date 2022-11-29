0a5707446b17bb0ba7b2a590d041db0795b4550a
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-11-29 16:37:01 +0100

    milestone 1 done

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+function randomPassword()
+{
+    $pswlength = $_POST['pswlength']; //Ci prendiamo la lunghezza della password dal form
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!£%&/?_-.,#@'; //Lista dei caratteri che comporranno la password
+    $alphalength = strlen($alphabet); //Inseriamo la lunghezza dei caratteri sopra dichiarati per utilizzarla nel 'rand'
+    $pass = array(); //Dichiariamo che pass è un array
+    for ($i = 0; $i < $pswlength; $i++) {
+        $n = rand(0, $alphalength); // Delimitiamo il range cosicchè tutti i caratteri possano venir presi in considerazione
+        $pass[] = $alphabet[$n]; // Creiamo la password
+
+    }
+    return implode($pass); // Ritorniamo la password, con implode trasformiamo un array in una stringa
+}
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,7 +35,7 @@
 <body class="bg-info">
     <div class="container">
         <?php
-        echo $_POST['pswlength'];
+        echo randomPassword();
         ?>
 
         <form action="index.php" method="POST" name="emailform">
